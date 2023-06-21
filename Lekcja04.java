8e4a8db28067fbd6b65175639b062002eb3298ff
PiotrPawlik002 <137320909+PiotrPawlik002@users.noreply.github.com>
2023-06-21 19:12:21 +0200

    Dodawanie dwóch plików źródłowych
    Pliki zawierają fragmenty kodu java.

---
+++ Lekcja04.java
@@ -0,0 +1,20 @@
+public class Lekcja04 {
+    public static void main(String[] args){
+        int a = 5, b = 6;
+        boolean logiczna = a!=b && a<=b;
+        System.out.println(logiczna);
+    }
+    /* Operatory porównania:
+        == rownosc
+        <> wiekszosc mniejszosc
+        <= wieksze lub rowne
+        >= mniejsze lub rowne
+        != nierowne
+       Operatory logiczne:
+       || lub
+       && oraz
+       ! negacja np. !(a = b)
+
+    */
+
+}
---
+++ Lekcja16.java
@@ -0,0 +1,26 @@
+public class Lekcja16 {
+    public static void main(String[] args){
+        int liczba = 50;
+        int liczba2 = liczba;
+        liczba2 = 111;
+        System.out.println("Liczba przed: " + liczba);
+        zmien(liczba);
+        System.out.println("Liczba po: " + liczba);
+
+        Liczba oliczba = new Liczba();
+        oliczba.liczba = 50;
+        Liczba oliczba2 = oliczba;
+        oliczba2.liczba = 111;
+        System.out.println("Obiekt liczby przed: " + oliczba.liczba);
+        zmien(oliczba);
+        System.out.println("Obiekt liczby po: " + oliczba.liczba);
+
+    }
+    public static void zmien(int liczba){           //proste metody kopiuja wartosci na rzecz swoich obliczen, po opuszczeniu metody jest niszcz.
+        liczba = -1;
+    }
+    public static void zmien(Liczba oliczba){
+        oliczba.liczba = -1;
+    }
+
+}
